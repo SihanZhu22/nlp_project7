af4a96eb32fd14e688e34355a1e644aeceeab2b9
Sihan Zhu <30918855+SihanZhu22@users.noreply.github.com>
2020-01-09 21:05:06 +0200

    improve the coefficient by text processing
    Also changed datamuse API output

--- main.py
+++ main.py
@@ -1,32 +1,148 @@
 from utils import *
 
-word_sim = simple_word_similarity('boat', 'ship')
-print('Example of word similarity: ', word_sim)
+# part 3
+datasets = ['mc', 'rg', 'wordsim']
 
-sentence_sim = simple_sentence_similarity('float boat with a high human', 'a dog in a big ship')
-print('Example of sentence similarity: ', sentence_sim)
+for data in datasets:
+    print('processing dataset: %s' % data)
+    with open('datasets/%s.csv' % data, newline='') as csvfile:
+        contents = list(csv.reader(csvfile, delimiter=';'))
 
-with open('mc.csv', newline='') as csvfile:
-    mc_28 = list(csv.reader(csvfile,delimiter=';'))
+    #Change max number of results from DatamuseAPI from here
+    sim_cal = np.array(words_similarity_dataset(contents, max_num=1000))
+    sim_ref = np.array(contents)[:,2].astype(float)
+    corr = pearson_correlation(sim_cal,sim_ref)
 
-mc_28 = np.array(mc_28)
+    with open('word_similarity.txt', 'a') as simfile:
+        simfile.write('Using Datamuse methods\n')
+        simfile.write('w1; w2; human_sim; method_sim\n\n')
+        for i, pair in enumerate(contents):
+            simfile.write('%s;%s;%s;%f\n' % (pair[0], pair[1], pair[2], sim_cal[i]))
+        simfile.write('\n\n')
 
-#list of calculated similarities
-#Optimal condition is max_num = 100
-sim_cal = np.array(words_similarity_dataset(mc_28,max_num = 100))#Change max number of results from DatamuseAPI from here
-#list of reference similarities
-sim_ref = mc_28[:,2]
-sim_ref = sim_ref.astype(float)
+    with open('results.txt', 'a') as resfile:
+        resfile.write('pearson correlation in dataset [%s] for Datamuse methods is %f\n' % (data, corr))
+
+exit()
+
+# part 5
+with open('datasets/stss-131.csv', encoding='utf8', newline='') as csvfile:
+    contents = list(csv.reader(csvfile, delimiter=';'))
+
+sim_cal = np.array(sentence_similarity_dataset(contents)).reshape(-1,)
+
+with open('sentence_similarity.txt', 'a') as simfile:
+    simfile.write('Using Datamuse methods\n')
+    simfile.write('s1; s2; human_sim; method_sim\n\n')
+    for i, pair in enumerate(contents):
+        simfile.write('%s;%s;%s;%f\n' % (pair[0], pair[1], pair[2], sim_cal[i] * 4))
+    simfile.write('\n\n')
+
+sim_ref = np.array(contents)[:,2].astype(float) / 4.0
 corr = pearson_correlation(sim_cal,sim_ref)
 
-with open('STSSa.csv', newline='') as csvfile:
-    stss = list(csv.reader(csvfile,delimiter=';'))
+with open('results.txt', 'a') as resfile:
+    resfile.write('pearson correlation in dataset [%s] for Datamuse methods is %f\n' % ('STS-131', corr))
 
-stss = np.array(stss)
+# part 6
+from gensim.models import Word2Vec
+import nltk
+nltk.download('brown')
 
-sim_cal_sen = np.array(sentence_similarity_dataset(stss))
-sim_ref_sen = stss[:,2]
-sim_ref_sen = sim_ref_sen.astype(float)
-for sim in sim_ref_sen:
-    sim = sim/4
-corr_sen = pearson_correlation(sim_cal_sen,sim_ref_sen)
+from nltk.corpus import brown
+
+with open('datasets/stss-131.csv', newline='') as csvfile:
+    contents = list(csv.reader(csvfile, delimiter=';'))
+
+model_word2vec = Word2Vec(brown.sents(), min_count=8)
+sim_cal = np.array(sentence_similarity_dataset_model(contents, model_word2vec.wv)).reshape(-1,)
+
+with open('sentence_similarity.txt', 'a') as simfile:
+    simfile.write('Using Word2Vec embedding\n')
+    simfile.write('s1; s2; human_sim; method_sim\n\n')
+    for i, pair in enumerate(contents):
+        simfile.write('%s;%s;%s;%f\n' % (pair[0], pair[1], pair[2], sim_cal[i] * 4))
+    simfile.write('\n\n')
+
+sim_ref = np.array(contents)[:,2].astype(float) / 4.0
+corr = pearson_correlation(sim_cal,sim_ref)
+
+with open('results.txt', 'a') as resfile:
+    resfile.write('pearson correlation in dataset [%s] for Word2Vec embedding is %f\n' % ('STS-131', corr))
+
+
+# part 7_glove
+
+with open('datasets/stss-131.csv', newline='') as csvfile:
+    contents = list(csv.reader(csvfile, delimiter=';'))
+
+model_glove = {}
+with open("glove.6B.50d.txt", 'r') as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = np.asarray(values[1:], "float32")
+        model_glove[word] = vector
+sim_cal = np.array(sentence_similarity_dataset_model(contents, model_glove)).reshape(-1,)
+
+with open('sentence_similarity.txt', 'a') as simfile:
+    simfile.write('Using Glove embedding\n')
+    simfile.write('s1; s2; human_sim; method_sim\n\n')
+    for i, pair in enumerate(contents):
+        simfile.write('%s;%s;%s;%f\n' % (pair[0], pair[1], pair[2], sim_cal[i] * 4))
+    simfile.write('\n\n')
+
+sim_ref = np.array(contents)[:,2].astype(float) / 4.0
+corr = pearson_correlation(sim_cal,sim_ref)
+
+with open('results.txt', 'a') as resfile:
+    resfile.write('pearson correlation in dataset [%s] for Glove embedding is %f\n' % ('STS-131', corr))
+
+# part 7_fasttext
+from gensim.models.fasttext import FastText
+import nltk
+nltk.download('brown')
+
+from nltk.corpus import brown
+
+with open('datasets/stss-131.csv', newline='') as csvfile:
+    contents = list(csv.reader(csvfile, delimiter=';'))
+
+model_fasttext = FastText(brown.sents(), min_count=8)
+sim_cal = np.array(sentence_similarity_dataset_model(contents, model_fasttext.wv)).reshape(-1,)
+
+with open('sentence_similarity.txt', 'a') as simfile:
+    simfile.write('Using FastText embedding\n')
+    simfile.write('s1; s2; human_sim; method_sim\n\n')
+    for i, pair in enumerate(contents):
+        simfile.write('%s;%s;%s;%f\n' % (pair[0], pair[1], pair[2], sim_cal[i] * 4))
+    simfile.write('\n\n')
+
+sim_ref = np.array(contents)[:,2].astype(float) / 4.0
+corr = pearson_correlation(sim_cal,sim_ref)
+
+with open('results.txt', 'a') as resfile:
+    resfile.write('pearson correlation in dataset [%s] for FastText embedding is %f\n' % ('STS-131', corr))
+
+# part 8
+with open('datasets/stss-131.csv', newline='') as csvfile:
+    contents = list(csv.reader(csvfile, delimiter=';'))
+
+from sematch.semantic.similarity import YagoTypeSimilarity
+sim = YagoTypeSimilarity()
+sim_cal = np.array(sentence_similarity_dataset_yago(contents, sim)).reshape(-1,)
+
+with open('sentence_similarity.txt', 'a') as simfile:
+    simfile.write('Using Yago concepts\n')
+    simfile.write('s1; s2; human_sim; method_sim\n\n')
+    for i, pair in enumerate(contents):
+        simfile.write('%s;%s;%s;%f\n' % (pair[0], pair[1], pair[2], sim_cal[i] * 4))
+    simfile.write('\n\n')
+
+sim_ref = np.array(contents)[:,2].astype(float) / 4.0
+corr = pearson_correlation(sim_cal,sim_ref)
+
+with open('results.txt', 'a') as resfile:
+    resfile.write('pearson correlation in dataset [%s] for Yago concepts is %f\n' % ('STS-131', corr))
+
+print('done')
--- utils.py
+++ utils.py
@@ -5,7 +5,12 @@ from scipy.stats import pearsonr
 import numpy as np
 import csv
 import pandas as pd
-
+from sklearn.metrics.pairwise import cosine_similarity
+import re
+import nltk
+from nltk import word_tokenize
+from nltk.stem import *
+from nltk.corpus import stopwords
 
 # part 1
 def datamuse(query, max_num):
@@ -20,44 +25,69 @@ def datamuse(query, max_num):
 
     return words
 
+def datamuse_v2(query, max_num, setting= ['words?ml', 'words?rel_spc', 'words?rel_gen']):
+    words = []
+    for info in setting:
+        url = 'https://api.datamuse.com/%s=%s&max=%d' % (info, query, max_num)
+        response = requests.get(url)
+        results = response.text
+
+        results = json.loads(results)
+
+        words.extend([a['word'] for a in results])
+
+    return words
+
+if '__main__' == __name__:
+
+    setting = ['words?ml', 'words?rel_spc', 'words?rel_gen', 'words?rel_par', 'words?rel_bga', 'words?rel_bgb', 'sug?s']
+
+    output = datamuse_v2('dog', 1000, setting)
+    print(output)
+
 # part 2
 def jaccard_similarity(a, b):
     overlap = [element for element in a if element in b]
     return 1.0 * len(overlap) / ((len(a) + len(b)) - len(overlap))
 
-def simple_word_similarity(w1, w2, max_num=30):
+def simple_word_similarity(w1, w2, max_num=1000):
     assert(isinstance(w1, str)), 'the first input parameter is not a string variable'
     assert(isinstance(w2, str)), 'the second input parameter is not a string variable'
 
-    words_1 = datamuse(w1, max_num=max_num)
-    words_2 = datamuse(w2, max_num=max_num)
+    words_1 = datamuse_v2(w1, max_num=max_num)
+    words_2 = datamuse_v2(w2, max_num=max_num)
 
     return jaccard_similarity(words_1, words_2)
 
-#part3
-#define the function to calculate all the similarities of one dataset
-def words_similarity_dataset(Dataset,max_num=30):
-    sim_list = []
-    for word_pair in Dataset:
-        w1 = word_pair[0]
-        w2 = word_pair[1]
-        similarity = simple_word_similarity(w1,w2,max_num = max_num)
-        sim_list.append(similarity)
-    return sim_list
-
-#function to calculate pearson coeffficients
+# part 3: function to calculate pearson coeffficients
 def pearson_correlation(data1, data2):
-# calculate Pearson's correlation
     corr, _ = pearsonr(data1, data2)
     print('Pearsons correlation: %.3f' % corr)
+    return corr
 
 # part 4
+#function to preprocess words
+def sentence_preprocess(sentence):
+    #tokenize
+    tokens = word_tokenize(sentence)
+    #stop-word removal
+    stop_words = set(stopwords.words('english'))
+    filtered_sentence = [w for w in tokens if not w in stop_words]
+
+    return filtered_sentence
+#function to preprocess documents
 def simple_sentence_similarity(s1, s2):
-    s1 = s1.strip().split()
-    s2 = s2.strip().split()
+    s1 = re.sub('[^a-zA-Z]', ' ', s1 )
+    s1 = re.sub('\s+', ' ', s1 )
+    s2 = re.sub('[^a-zA-Z]', ' ', s2 )
+    s2 = re.sub('\s+', ' ', s2 )
+    #s1 = s1.strip().split()
+    s1 = sentence_preprocess(s1)
+    #s2 = s2.strip().split()
+    s2 = sentence_preprocess(s2)
     overlap_1 = None
     for w in s1:
-        words = datamuse(w, 500)
+        words = datamuse_v2(w, 1000)
         if not overlap_1:
             overlap_1 = words
         else:
@@ -66,7 +96,7 @@ def simple_sentence_similarity(s1, s2):
 
     overlap_2 = None
     for w in s2:
-        words = datamuse(w, 500)
+        words = datamuse_v2(w, 1000)
         if not overlap_2:
             overlap_2 = words
         else:
@@ -74,17 +104,111 @@ def simple_sentence_similarity(s1, s2):
     overlap_2.extend(s2)
     return jaccard_similarity(overlap_1, overlap_2)
 
-if '__main__' == __name__:
-    word_sim = simple_word_similarity('boat', 'ship')
-    print('word similarity: ', word_sim)
+# Using datamuse
+def words_similarity_dataset(Dataset,max_num=1000):
+    sim_list = []
+    for i, word_pair in enumerate(Dataset):
+        print('%d/%d th pair' % (i + 1, len(Dataset)))
+        w1 = word_pair[0]
+        w2 = word_pair[1]
+        similarity = simple_word_similarity(w1, w2, max_num=max_num)
+        sim_list.append(similarity)
+    return sim_list
 
-    sentence_sim = simple_sentence_similarity('float boat with a high human', 'a dog in a big ship')
-    print('sentence similarity: ', sentence_sim)
-
-#part 5
 def sentence_similarity_dataset(Dataset):
     sim_list = []
-    for line in Dataset:
-        similarity = simple_sentence_similarity(line[0], line[1])
+    for i, sentence_pair in enumerate(Dataset):
+        print('%d/%d th pair' % (i + 1, len(Dataset)))
+        similarity = simple_sentence_similarity(sentence_pair[0], sentence_pair[1])
+        sim_list.append(similarity)
+    return sim_list
+
+
+# Using Word2Vec, Glove, FastText
+def model_sentence_similarity(s1, s2, model):
+    s1 = re.sub('[^a-zA-Z]', ' ', s1 )
+    s1 = re.sub('\s+', ' ', s1 )
+    s2 = re.sub('[^a-zA-Z]', ' ', s2 )
+    s2 = re.sub('\s+', ' ', s2 )
+    #s1 = s1.strip().split()
+    s1 = sentence_preprocess(s1)
+    #s2 = s2.strip().split()
+    s2 = sentence_preprocess(s2)
+    vec_1 = None
+    for w in s1:
+        if vec_1 is None:
+            try:
+                vec_1 = model[w]
+            except Exception as e:
+                pass
+        else:
+            try:
+                vec_1 += model[w]
+            except Exception as e:
+                pass
+    vec_1 = vec_1 / len(s1)
+
+    vec_2 = None
+    for w in s2:
+        if vec_2 is None:
+            try:
+                vec_2 = model[w]
+            except Exception as e:
+                pass
+        else:
+            try:
+                vec_2 += model[w]
+            except Exception as e:
+                pass
+    vec_2 = vec_2 / len(s2)
+    return cosine_similarity(vec_1.reshape(1, -1), vec_2.reshape(1, -1))
+
+def sentence_similarity_dataset_model(Dataset, model):
+    sim_list = []
+    for i, sentence_pair in enumerate(Dataset):
+        print('%d/%d th pair' % (i + 1, len(Dataset)))
+        similarity = model_sentence_similarity(sentence_pair[0], sentence_pair[1], model)
+        sim_list.append(similarity)
+    return sim_list
+
+
+# Using Yago
+def yago_word_similarity(w1, w2, sim):
+    concepts_1 = sim.word2yago(w1)
+    concepts_2 = sim.word2yago(w2)
+    if concepts_1 and concepts_2:
+        return np.array([sim.yago_similarity(c1, c2) for c1 in concepts_1 for c2 in concepts_2]).max()
+    else:
+        return 0
+
+def sentence_preprocess_2(sentence):
+    #stemming
+    porter = PorterStemmer()
+    stemmed_sentence = porter.stem(sentence)
+    #sentence = sentence.lower()
+    #tokenize
+    tokens = word_tokenize(stemmed_sentence)
+    #stop-word removal
+    stop_words = set(stopwords.words('english'))
+    filtered_sentence = [w for w in tokens if not w in stop_words]
+
+    return filtered_sentence
+
+def yago_sentence_similarity(s1, s2, sim):
+    s1 = re.sub('[^a-zA-Z]', ' ', s1 )
+    s1 = re.sub('\s+', ' ', s1 )
+    s2 = re.sub('[^a-zA-Z]', ' ', s2 )
+    s2 = re.sub('\s+', ' ', s2 )
+    #s1 = s1.strip().split()
+    s1 = sentence_preprocess_2(s1)
+    #s2 = s2.strip().split()
+    s2 = sentence_preprocess_2(s2)
+    return np.array([yago_word_similarity(w1, w2, sim) for w1 in s1 for w2 in s2]).mean()
+
+def sentence_similarity_dataset_yago(Dataset, sim):
+    sim_list = []
+    for i, sentence_pair in enumerate(Dataset):
+        print('%d/%d th pair' % (i + 1, len(Dataset)))
+        similarity = yago_sentence_similarity(sentence_pair[0], sentence_pair[1], sim)
         sim_list.append(similarity)
     return sim_list
